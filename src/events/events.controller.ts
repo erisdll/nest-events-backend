966fb2ed75d8b9fd7aabac9b8c6908ee701fae02
erisdll <72031082+erisdll@users.noreply.github.com>
2023-11-26 22:10:46 -0300

    update

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,17 +1,25 @@
-import { Body, Controller, Post, Get, Patch, Delete, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Param,
+} from '@nestjs/common';
 import { EventsService } from './events.service';
-import { CreateEventDto} from './dto/create-event.dto';
-import { UpdateEventDto} from './dto/update-event.dto';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
 
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
-  
+
   @Post()
   create(@Body() createEventDto: CreateEventDto) {
     return this.eventsService.create(createEventDto);
   }
-  
+
   @Get()
   findAll() {
     return this.eventsService.findAll();
@@ -19,16 +27,16 @@ export class EventsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.eventsService.findOne(+id)
+    return this.eventsService.findOne(+id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
-    return this.eventsService.update(+id, updateEventDto)
+    return this.eventsService.update(+id, updateEventDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.eventsService.remove(+id)
+    return this.eventsService.remove(+id);
   }
 }
