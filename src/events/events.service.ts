9b8ea2f3f80567e74d2b8da101eb51e7363c846d
erisdll <72031082+erisdll@users.noreply.github.com>
2023-11-26 22:11:49 -0300

    updated to repositoyry pattern

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Event } from './entities/event.entity';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 
 @Injectable()
 export class EventsService {
+  constructor(
+    @InjectRepository(Event)
+    private eventRepository: Repository<Event>,
+  ) {}
   create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+    return this.eventRepository.save(createEventDto);
   }
 
-  findAll() {
-    return `This action returns all events`;
+  findAll(): Promise<Event[] | null> {
+    return this.eventRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} event`;
+  findOne(id: number): Promise<Event | null> {
+    return this.eventRepository.findOneBy({ id });
   }
 
   update(id: number, updateEventDto: UpdateEventDto) {
-    return `This action updates a #${id} event`;
+    return this.eventRepository.update(id, updateEventDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} event`;
+    return this.eventRepository.delete(id);
   }
 }
