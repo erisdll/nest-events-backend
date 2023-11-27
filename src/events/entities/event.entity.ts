45082e10b871fab84b00b13b0839744add634e3f
erisdll <72031082+erisdll@users.noreply.github.com>
2023-11-26 21:57:26 -0300

    updated event entity to typeorm defs

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,7 +1,19 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
 export class Event {
+  @PrimaryColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column()
   date: Date;
+
+  @Column()
   location: string;
 }
